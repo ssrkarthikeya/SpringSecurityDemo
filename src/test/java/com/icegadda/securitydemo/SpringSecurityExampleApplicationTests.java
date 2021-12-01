b4bc58d52ae36564d935d175362aa910d3456305
ssrkarthikeya <Hachinov1>
2021-12-01 09:41:11 +0530

    intial

---
+++ src/test/java/com/icegadda/securitydemo/SpringSecurityExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.icegadda.securitydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
